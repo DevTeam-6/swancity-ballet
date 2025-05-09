fefe01f6012b5b944509e0546813b573309b4b55
James <jnr1298@aol.com>
2025-05-08 23:09:41 -0400

    Adding/modifying routes in main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
 from bson import json_util
@@ -18,13 +18,34 @@ db = client.get_database()  # Use the database defined in the URI or default
 def home():
     return {"message": "Welcome to the App!"}
 
-@app.route('/data', methods=['GET'])
-def get_data():
+@app.route('/students', methods=['GET'])
+def get_students():
     """Fetch all documents from a collection."""
     data = db.students.find()
     result = list(data)  
 
     return json_util.dumps({"data": result}), 200, {"Content-Type": "application/json"}
+
+@app.route('/api/families', methods=['GET'],)
+def get_():
+    """Fetch all documents from a collection."""
+    data = db.families.find()
+    result = list(data)  
+
+    return json_util.dumps({"data": result}), 200, {"Content-Type": "application/json"}
+
+@app.route('/families/create', methods=['POST'])
+def create_family():
+    # Get form data from the request body (which is a JSON object)
+    family_data = request.get_json()
+
+    # Insert the new family document into MongoDB
+    family_collection = db.families  # Use the 'families' collection in your DB
+    result = family_collection.insert_one(family_data)
+
+    # Return the newly created family with the MongoDB _id
+    family_data['_id'] = str(result.inserted_id)
+    return jsonify(family_data), 201
    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
